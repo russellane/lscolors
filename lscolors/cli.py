1c0fe62195902929c4cc765cee578b2fe2a1ac55
Russel Lane <russel@rlane.com>
2021-12-15 13:47:09 -0600

    move imports

--- lscolors/cli.py
+++ lscolors/cli.py
@@ -5,13 +5,7 @@ import sys
 
 import argcomplete
 
-import lscolors.commands.chart
-import lscolors.commands.check
-import lscolors.commands.configs
-import lscolors.commands.docs
-import lscolors.commands.report
-import lscolors.commands.samples
-import lscolors.commands.sort
+import lscolors.commands
 from lscolors.__version__ import __version__
 
 
